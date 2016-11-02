6c0bb6e837982537d5b22f7967b525b1c6e38bf6
Fabian Köster <mail@fabian-koester.com>
2016-11-02 21:02:31 +0100

    Fix copy and paste mistake

--- create_dhparam.sh
+++ create_dhparam.sh
@@ -1,6 +1,6 @@
 if [ ! -f /etc/dhparam/dhparam.pem ]; then
 
-    echo "dhparam file /etc/nginx/dhparam/dhparam.pem does not exist. Generating one with 4086 bit. This will take a while..."
+    echo "dhparam file /etc/dhparam/dhparam.pem does not exist. Generating one with 4086 bit. This will take a while..."
     openssl dhparam -out /etc/dhparam/dhparam.pem 4096 || die "Could not generate dhparam file"
-    echo "Finished. Starting nginx now..."
+    echo "Finished. Starting service now..."
 fi
